2ccae268c1e3c98b36085e3e2d494e4dff0b8317
keksikls <151632212+keksikls@users.noreply.github.com>
2024-10-31 07:27:53 +0300

    use generics
    use generics

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace использование_дженериков
+{
+    public class Product<T>
+    { 
+        public T Price { get; set; }
+        public string Name { get; set; }
+
+        public Product(string name, T price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void sell() 
+        {
+            Console.WriteLine("Вы продали " + Name + " за " + Price);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product<double> cheese = new Product<double>("Чедер", 150.34);
+            cheese.sell();
+            Console.ReadLine();
+        }
+    }
+}
